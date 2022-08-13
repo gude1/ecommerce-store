23ede293cbe64ad73870315c3c9edf8dd190442d
Gideon <owolabigidslab@gmail.com>
2022-08-13 02:17:52 +0100

    home page design completed, moving to product details page

--- src/components/SubHeader/SubHeader.js
+++ src/components/SubHeader/SubHeader.js
@@ -1,12 +1,32 @@
 import React from "react";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 import "../../App.css";
 import "./SubHeader.css";
 
 export default function SubHeader() {
+  const options = ["All", "Electronics", "Bags"];
   return (
     <header className="category-header slideInRight">
       <div className="productdesc-ctn">
         <h3>Product List</h3>
+      </div>
+      <div>
+        <Dropdown
+          options={options}
+          placeholder="Product Category"
+          controlClassName="category-select-control"
+          arrowOpen={
+            <span className="material-symbols-outlined category-select-arrow">
+              arrow_drop_up
+            </span>
+          }
+          arrowClosed={
+            <span class="material-symbols-outlined category-select-arrow">
+              arrow_drop_down
+            </span>
+          }
+        />
       </div>
     </header>
   );
