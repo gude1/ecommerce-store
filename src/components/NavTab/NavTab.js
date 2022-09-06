eb88878a515a6a71d05ba6daf3755805f9153ad1
Gideon <owolabigidslab@gmail.com>
2022-09-06 18:56:37 +0100

    added nav button to dashboard sidebar

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../ui/Button/Button";
 import "./NavTab.css";
 
 function NavTab() {
@@ -6,11 +7,51 @@ function NavTab() {
     <div className="dashboard-sidebar-ctn">
       <div className="dashboard-sidebar-logo-ctn">
         <img
-          className="dashboard-header-logo"
+          className="dashboard-header-logo dashboard-sidebar-logo"
           src={
             "https://dashtar-admin.vercel.app/static/media/logo-dark.acf69e90.svg"
-            // "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWqLkNic7vtDF-3QMUqg10oaN-hC-QYJUJI-kRcZx9Mg&s"
           }
+        />
+      </div>
+
+      <div className="dashboard-sidebar-mainctn">
+        <Button
+          className="dashboard-sidebar-navbtn"
+          leftIcon={"dashboard"}
+          leftIconClass={"dashboard-sidebar-navbtn-icon"}
+          title={"Dashboard"}
+          titleClass={"dashboard-sidebar-navbtn-title"}
+        />
+        <Button
+          className="dashboard-sidebar-navbtn"
+          leftIcon={"inventory_2"}
+          leftIconClass={"dashboard-sidebar-navbtn-icon"}
+          title={"Products"}
+          titleClass={"dashboard-sidebar-navbtn-title"}
+        />
+
+        <Button
+          className="dashboard-sidebar-navbtn"
+          leftIcon={"sell"}
+          leftIconClass={"dashboard-sidebar-navbtn-icon"}
+          title={"Orders"}
+          titleClass={"dashboard-sidebar-navbtn-title"}
+        />
+
+        <Button
+          className="dashboard-sidebar-navbtn"
+          leftIcon={"person_pin"}
+          leftIconClass={"dashboard-sidebar-navbtn-icon"}
+          title={"Customers"}
+          titleClass={"dashboard-sidebar-navbtn-title"}
+        />
+
+        <Button
+          className="dashboard-sidebar-navbtn"
+          leftIconClass={"dashboard-sidebar-navbtn-icon"}
+          leftIcon={"settings"}
+          title={"Settings"}
+          titleClass={"dashboard-sidebar-navbtn-title"}
         />
       </div>
     </div>
