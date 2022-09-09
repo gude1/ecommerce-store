d3c7d94cdff4dc6f499e442f49a888fb9ff08351
Gideon <owolabigidslab@gmail.com>
2022-09-09 14:04:31 +0100

    wip dashboard updates

--- src/pages/DashHome/DashHome.js
+++ src/pages/DashHome/DashHome.js
@@ -64,64 +64,68 @@ function DashHome() {
           style={{ backgroundColor: "var(--green)" }}
           title="Total Orders"
         />
-        <ResponsiveContainer width={300} height={250}>
-          <AreaChart data={staticgraphdata} margin={{ top: 0 }}>
-            <defs>
-              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="37.6%"
-                  stopColor="rgba(33, 194, 229, 0.7) "
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="102.68%"
-                  stopColor="rgba(33, 194, 229, 0)"
-                  stopOpacity={0}
-                />
-              </linearGradient>
-              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            <XAxis dataKey="name" axisLine={false} tickLine={false} />
+        <div className="dashboard-graph-ctn">
+          <ResponsiveContainer width={"100%"} height={250}>
+            <AreaChart data={staticgraphdata} margin={{ top: 0 }}>
+              <defs>
+                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="37.6%"
+                    stopColor="rgba(33, 194, 229, 0.7) "
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset="102.68%"
+                    stopColor="rgba(33, 194, 229, 0)"
+                    stopOpacity={0}
+                  />
+                </linearGradient>
+                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <XAxis dataKey="name" axisLine={false} tickLine={false} />
 
-            {/* <YAxis /> */}
-            <CartesianGrid strokeDasharray="0.2" />
-            <Tooltip
-              offset={5}
-              contentStyle={{
-                backgroundColor: "#4764E6",
-                border: 0,
-                color: "white",
-                height: "60px",
-                fontFamily: "Montserrat-Regular",
-                fontStyle: "normal",
-                fontWeight: "500",
-                fontSize: "12px",
-                lineHeight: "16px",
-              }}
-              itemStyle={{
-                color: "white",
-                fontFamily: "Montserrat-Regular",
-                fontStyle: "normal",
-                fontWeight: "500",
-                fontSize: "12px",
-                lineHeight: "16px",
-              }}
-              // cursor={false}
-              formatter={(value, name, props) => [<span>&#8358;{value}</span>]}
-            />
-            <Area
-              type="monotone"
-              dataKey="uv"
-              stroke={"#4764E6"}
-              strokeWidth={3}
-              fillOpacity={1}
-              fill="url(#colorUv)"
-            />
-          </AreaChart>
-        </ResponsiveContainer>
+              {/* <YAxis /> */}
+              <CartesianGrid strokeDasharray="0.2" />
+              <Tooltip
+                offset={5}
+                contentStyle={{
+                  backgroundColor: "#4764E6",
+                  border: 0,
+                  color: "white",
+                  height: "60px",
+                  fontFamily: "Montserrat-Regular",
+                  fontStyle: "normal",
+                  fontWeight: "500",
+                  fontSize: "12px",
+                  lineHeight: "16px",
+                }}
+                itemStyle={{
+                  color: "white",
+                  fontFamily: "Montserrat-Regular",
+                  fontStyle: "normal",
+                  fontWeight: "500",
+                  fontSize: "12px",
+                  lineHeight: "16px",
+                }}
+                // cursor={false}
+                formatter={(value, name, props) => [
+                  <span>&#8358;{value}</span>,
+                ]}
+              />
+              <Area
+                type="monotone"
+                dataKey="uv"
+                stroke={"#4764E6"}
+                strokeWidth={3}
+                fillOpacity={1}
+                fill="url(#colorUv)"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </div>
   );
