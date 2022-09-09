8d978c875506a101542592b5d37b62212856a182
Gideon <owolabigidslab@gmail.com>
2022-09-09 13:33:17 +0100

    wip dashboard updates

--- src/pages/DashHome/DashHome.js
+++ src/pages/DashHome/DashHome.js
@@ -1,10 +1,48 @@
 import React from "react";
 import SubHeader from "../../components/SubHeader/SubHeader";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  XAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 import "../../App.css";
 import "./DashHome.css";
 import Panel from "../../components/Panel/Panel";
 
 function DashHome() {
+  const staticgraphdata = [
+    {
+      name: "SUN",
+      uv: 0,
+    },
+    {
+      name: "MON",
+      uv: 0,
+    },
+    {
+      name: "TUE",
+      uv: 2000,
+    },
+    {
+      name: "WED",
+      uv: 0,
+    },
+    {
+      name: "THUR",
+      uv: 0,
+    },
+    {
+      name: "FRI",
+      uv: 0,
+    },
+    {
+      name: "SAT",
+      uv: 0,
+    },
+  ];
   return (
     <div>
       <SubHeader
@@ -12,22 +50,78 @@ function DashHome() {
         className="dashboard-subheader"
         titleClassName="dashboard-subheader-title"
       />
-      <div className="slideInUp">
-        <div className="dash-home-first-section">
-          <Panel icon="sell" className="dash-home-panel" title="Today Orders" />
-          <Panel
-            icon="sell"
-            className="dash-home-panel"
-            style={{ backgroundColor: "var(--blue)" }}
-            title="This Month"
-          />
-          <Panel
-            icon="sell"
-            className="dash-home-panel"
-            style={{ backgroundColor: "var(--green)" }}
-            title="Total Orders"
-          />
-        </div>
+      <div className="slideInUp dash-home">
+        <Panel icon="sell" className="dash-home-panel" title="Today Orders" />
+        <Panel
+          icon="sell"
+          className="dash-home-panel"
+          style={{ backgroundColor: "var(--blue)" }}
+          title="This Month"
+        />
+        <Panel
+          icon="sell"
+          className="dash-home-panel"
+          style={{ backgroundColor: "var(--green)" }}
+          title="Total Orders"
+        />
+        <ResponsiveContainer width={300} height={250}>
+          <AreaChart data={staticgraphdata} margin={{ top: 0 }}>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="37.6%"
+                  stopColor="rgba(33, 194, 229, 0.7) "
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="102.68%"
+                  stopColor="rgba(33, 194, 229, 0)"
+                  stopOpacity={0}
+                />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="name" axisLine={false} tickLine={false} />
+
+            {/* <YAxis /> */}
+            <CartesianGrid strokeDasharray="0.2" />
+            <Tooltip
+              offset={5}
+              contentStyle={{
+                backgroundColor: "#4764E6",
+                border: 0,
+                color: "white",
+                height: "60px",
+                fontFamily: "Montserrat-Regular",
+                fontStyle: "normal",
+                fontWeight: "500",
+                fontSize: "12px",
+                lineHeight: "16px",
+              }}
+              itemStyle={{
+                color: "white",
+                fontFamily: "Montserrat-Regular",
+                fontStyle: "normal",
+                fontWeight: "500",
+                fontSize: "12px",
+                lineHeight: "16px",
+              }}
+              // cursor={false}
+              formatter={(value, name, props) => [<span>&#8358;{value}</span>]}
+            />
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke={"#4764E6"}
+              strokeWidth={3}
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
