358901ea1089364a741a15c124df5207b87b6c6d
Gideon <owolabigidslab@gmail.com>
2022-09-10 11:45:22 +0100

    wip dashhome

--- src/pages/DashHome/DashHome.js
+++ src/pages/DashHome/DashHome.js
@@ -96,7 +96,7 @@ function DashHome() {
               }
             />
           </div>
-          <ResponsiveContainer width={"100%"} height={250}>
+          <ResponsiveContainer width={"100%"} height={220}>
             <AreaChart data={staticgraphdata} margin={{ top: 0 }}>
               <defs>
                 <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
