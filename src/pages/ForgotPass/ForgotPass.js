a87ec89509d0556c444bc44c01f36748e21e8c85
Gideon <owolabigidslab@gmail.com>
2022-09-11 02:13:10 +0100

    completed forgotpassword page design

---
+++ src/pages/ForgotPass/ForgotPass.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../../components/ui/Button/Button";
+import Input from "../../components/ui/Input/Input";
+import "./ForgotPass.css";
+
+function ForgotPass() {
+  return (
+    <div className="auth-ctn slideInUp">
+      <div className="authform-ctn">
+        <div className="authform-picctn"></div>
+        <div className="authform-detailctn">
+          <form className="authform" onSubmit={(e) => e.preventDefault()}>
+            <span className="authform-title">Forgot password</span>
+            <Input
+              placeholder={"***@mail.com"}
+              label={"Email"}
+              className="authform-inputctn"
+              labelClassName="authform-input-label"
+              inputProps={{
+                className: "authform-input",
+                type: "email",
+              }}
+            />
+            <Button title="Submit" className="authform-actionbtn" />
+
+            <Link className="authform-link" to={"/auth"}>
+              Create Account
+            </Link>
+
+            <Link className="authform-link" to={"signin"} replace>
+              Login
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ForgotPass;
